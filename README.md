4388ae4ea6458740afa1c5d2917206a725a22c16
BytePanther <BytePanther@outlook.com>
2024-03-22 15:28:24 -0400

    Deserialize JSON response - version 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # catscii
 
 Serves cat pictures as ASCII art over the internet.
+
+Following along from section 3 in this series: 
+https://fasterthanli.me/series/building-a-rust-service-with-nix/part-3
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CatImage {
+    id: String,
+    // this is the only field we will really need, but let's show how we would
+    // deserialize the other fields as well
+    url: String,
+    width: usize,
+    height: usize,
+}
+
+
 #[tokio::main]
 async fn main() {
     let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
     .await
     .unwrap();
     
-    println!("Status: {}", res.status());
-    
-    let body = res.text().await.unwrap();
-    
-    println!("Body: {}", body);  
+    if !res.status().is_success() {
+        panic!("Request failed with HTTP code / status: {}", res.status());
+    }
+
+    //
+    let images: Vec<CatImage> = res.json().await.unwrap();
+    let image = images
+    .first()
+    .expect("The cat API should return at least one image");
+
+    println!("The image is at {}", image.url);
+
 }
