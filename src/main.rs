9e586e9d180adc1d52d518ccde09ba66de552d34
BytePanther <BytePanther@outlook.com>
2024-03-22 16:19:25 -0400

    we got cats

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ use serde::Deserialize;
 #[tokio::main]
 
 async fn main() {
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    // only dump the first 200 bytes so our terminal survives the
-    // onslaught of bytes. this will panic if the image has fewer than 200 bytes
-    println!("{:?}", &image_bytes[..200].hex_dump());
+    let art = get_cat_ascii_art().await.unwrap();
+    println!("{}", art);
 }
 
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
     // Define a struct to deserialize the JSON response
     #[derive(Deserialize)]
     struct CatImage {
@@ -30,12 +28,16 @@ async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The cat API returned no images"))?;
 
-    Ok(client
-        .get(&image.url)
+    let image_bytes = client
+        .get(image.url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
-        .await?
-        .to_vec())
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+
+    Ok(ascii_art)
 }
